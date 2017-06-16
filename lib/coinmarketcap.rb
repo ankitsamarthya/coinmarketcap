42b19e785d17e29a2fd1140b122fdd59edffaa0f
Ankit Samarthya <ankitsamar@outlook.com>
2017-06-17 01:08:01 +0530

    get all coins with and without limit

--- lib/coinmarketcap.rb
+++ lib/coinmarketcap.rb
@@ -1,5 +1,14 @@
-require "coinmarketcap/version"
+require 'coinmarketcap/version'
+require 'httparty'
 
 module Coinmarketcap
-  # Your code goes here...
+
+  def self.coins(limit = nil)
+    if limit.nil?
+      HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
+    else
+      HTTParty.get("https://api.coinmarketcap.com/v1/ticker/?limit=#{limit}")
+    end
+  end
+
 end
--- spec/coinmarketcap_spec.rb
+++ spec/coinmarketcap_spec.rb
@@ -1,11 +1,31 @@
 require "spec_helper"
 
-RSpec.describe Coinmarketcap do
+describe Coinmarketcap do
   it "has a version number" do
     expect(Coinmarketcap::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(true).to eq(true)
+  describe "#coins" do
+    context 'without limit' do
+      it "should receive a 200 response with all coins" do
+        VCR.use_cassette('all_coin_response') do
+          response = Coinmarketcap.coins
+          coins = JSON.parse(response.body)
+          expect(response.code).to eq(200)
+          expect(coins.count).to be > 0
+        end
+      end
+    end
+
+    context 'with limit as 10' do
+      it "should receive a 200 response with all coins" do
+        VCR.use_cassette('limit_coin_response') do
+          response = Coinmarketcap.coins(limit = 10)
+          coins = JSON.parse(response.body)
+          expect(response.code).to eq(200)
+          expect(coins.count).to eq(10)
+        end
+      end
+    end
   end
 end
