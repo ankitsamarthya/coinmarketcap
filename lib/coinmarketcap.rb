c3110eb66464cbd55538149308cf9d349f1a9435
Ankit S <ankit@weinvest.net>
2017-09-14 23:54:54 +0530

    big fix

--- lib/coinmarketcap.rb
+++ lib/coinmarketcap.rb
@@ -35,10 +35,10 @@ module Coinmarketcap
         price_bundle = {}
         each_row = Nokogiri::HTML(row.to_s).css('td')
         price_bundle[:date] = Date.parse(each_row[0].text)
-        price_bundle[:open] = each_row[0].text
-        price_bundle[:high] = each_row[1].text
-        price_bundle[:low] = each_row[2].text
-        price_bundle[:close] = each_row[3].text
+        price_bundle[:open] = each_row[1].text
+        price_bundle[:high] = each_row[2].text
+        price_bundle[:low] = each_row[3].text
+        price_bundle[:close] = each_row[4].text
         prices << price_bundle
       rescue => error
         next
--- lib/coinmarketcap/version.rb
+++ lib/coinmarketcap/version.rb
@@ -1,3 +1,3 @@
 module Coinmarketcap
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
